eb546b52aac02b5dca3d36db2a1cffe0cd55d526
Nikhi9122 <sukalanikhil945@gmail.com>
2023-08-01 12:33:40 +0530

    formikcomponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,55 @@
 
 import React from 'react';
-import {useFormik} from 'formik';
+import {Formik, Field, useFormik, ErrorMessage} from 'formik';
 import * as Yup from 'yup';
 function App() {
- var myform=useFormik({
-  initialValues:{
-    firstname:'',
-    lastname:'',
-    email:''
-  },
- validationSchema:Yup.object({
-  firstname:Yup.string().required("check"),
-  lastname:Yup.string().required('check'),
-  email:Yup.string().required("check")
 
- }),
-
- 
-  onSubmit:(x)=>{
-    console.log(x)
-  }
- })
- console.log(myform)
  return (
-
-  <div style={{border:'2px solid green',padding:'10px',margin:'10px'}}>
+    <div style={{border:'2px solid green',padding:'10px',margin:'10px'}}>
     <h1>FORMIK</h1>
     <div style={{border:'2px solid skyblue',padding:'10px',margin:'10px'}}>
-      <form onSubmit={myform.handleSubmit}>
-     <input type='text' name='firstname' onChange={myform.handleChange} onBlur={myform.handleBlur}/><br />
-     <i>{myform.touched.firstname && myform.errors.firstname}</i><br />
-     <input type='text' name='lastname' onChange={myform.handleChange} onBlur={myform.handleBlur}/><br />
-     <i>{myform.touched.lastname && myform.errors.lastname}</i><br />
-     <input type='email' name='email' onChange={myform.handleChange} onBlur={myform.handleBlur}/><br />
-     <i>{myform.touched.email && myform.errors.email}</i><br />
-     {JSON.stringify(myform.touched)}
-     <button >SAVE</button>
-     </form>
+       <Formik 
+                initialValues={{
+                firstname:'',
+                lastname:'',
+                email:''
+              }}
+                  validationSchema={Yup.object({
+                  firstname:Yup.string().required("check fname"),
+                  lastname:Yup.string().required('check lname '),
+                  email:Yup.string().required("check email")
+            
+            })}
+            
+            
+                onSubmit={(x)=>{
+                console.log(x)
+              }}
+       >
+        {
+        (myform)=>{
+          console.log(myform)
+                      return(
+                        <form onSubmit={myform.handleSubmit}>
+                          <Field name='firstname'></Field>
+                          <ErrorMessage name='firstname' component='i'></ErrorMessage>
+                          <br />
+                          <Field name='lastname'></Field>
+                          <ErrorMessage name='lastname' component='i'></ErrorMessage>
+                          <br />
+                          <Field type='email' name='email'></Field>
+                          <ErrorMessage name='email' component='i'></ErrorMessage><br />
+
+                          <button type='submit'>SAVE</button>
+                        </form>
+                      )
+
+        }
+      }
+       </Formik>
+     
+     
+
     </div>
     
     
